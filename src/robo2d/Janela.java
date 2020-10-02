0483159c1ff6a3665a8b745ec144185c9829fa6f
DiegoPSantana <diego.fourstage@gmail.com>
2020-10-02 20:49:11 -0300

    Criado janela JFrame

--- src/robo2d/Janela.java
+++ src/robo2d/Janela.java
@@ -1,5 +1,7 @@
 package robo2d;
 
+import javax.swing.JFrame;
+
 /**
  *
  * @author D1350
@@ -7,7 +9,15 @@ package robo2d;
 public class Janela {
 
     public static void main(String[] args) {
-        
+
+        JFrame janela = new JFrame("Mr.Robot");
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setSize(1280, 720);
+        janela.setLocationRelativeTo(null);
+        janela.setResizable(false);
+
+        janela.setVisible(true);
+
     }
-    
+
 }
