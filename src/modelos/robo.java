2db4eb0062f1060d503b39c99e50f1da499ba492
DiegoPSantana <diego.fourstage@gmail.com>
2020-10-02 21:57:37 -0300

    Adicionado classe atributos getters e setters

---
+++ src/modelos/robo.java
@@ -0,0 +1,182 @@
+package modelos;
+/**
+ *
+ * @author D1350
+ */
+public class robo {  
+    
+    private int runAtual = 0;
+    private int idleAtual = 0;
+    private int timer;
+    private int largura;
+    private int altura;
+    private int direcao;
+    private int ultimaDirecao;
+    private int posX;
+    private int posY;
+    private int velocidade;
+    private int puloAtual = 0;
+    private boolean pulo;
+    private int alturaPulo;
+    private int deadAtual = 0;
+    private boolean dead;
+    private int atirouAtual = 0;
+    private boolean atirou;
+    private int tiroAtualParado;
+    private boolean atirouParado;
+
+    public int getRunAtual() {
+        return runAtual;
+    }
+
+    public void setRunAtual(int runAtual) {
+        this.runAtual = runAtual;
+    }
+
+    public int getIdleAtual() {
+        return idleAtual;
+    }
+
+    public void setIdleAtual(int idleAtual) {
+        this.idleAtual = idleAtual;
+    }
+
+    public int getTimer() {
+        return timer;
+    }
+
+    public void setTimer(int timer) {
+        this.timer = timer;
+    }
+
+    public int getLargura() {
+        return largura;
+    }
+
+    public void setLargura(int largura) {
+        this.largura = largura;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public int getDirecao() {
+        return direcao;
+    }
+
+    public void setDirecao(int direcao) {
+        this.direcao = direcao;
+    }
+
+    public int getUltimaDirecao() {
+        return ultimaDirecao;
+    }
+
+    public void setUltimaDirecao(int ultimaDirecao) {
+        this.ultimaDirecao = ultimaDirecao;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    public int getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(int velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    public int getPuloAtual() {
+        return puloAtual;
+    }
+
+    public void setPuloAtual(int puloAtual) {
+        this.puloAtual = puloAtual;
+    }
+
+    public boolean isPulo() {
+        return pulo;
+    }
+
+    public void setPulo(boolean pulo) {
+        this.pulo = pulo;
+    }
+
+    public int getAlturaPulo() {
+        return alturaPulo;
+    }
+
+    public void setAlturaPulo(int alturaPulo) {
+        this.alturaPulo = alturaPulo;
+    }
+
+    public int getDeadAtual() {
+        return deadAtual;
+    }
+
+    public void setDeadAtual(int deadAtual) {
+        this.deadAtual = deadAtual;
+    }
+
+    public boolean isDead() {
+        return dead;
+    }
+
+    public void setDead(boolean dead) {
+        this.dead = dead;
+    }
+
+    public int getAtirouAtual() {
+        return atirouAtual;
+    }
+
+    public void setAtirouAtual(int atirouAtual) {
+        this.atirouAtual = atirouAtual;
+    }
+
+    public boolean isAtirou() {
+        return atirou;
+    }
+
+    public void setAtirou(boolean atirou) {
+        this.atirou = atirou;
+    }
+
+    public int getTiroAtualParado() {
+        return tiroAtualParado;
+    }
+
+    public void setTiroAtualParado(int tiroAtualParado) {
+        this.tiroAtualParado = tiroAtualParado;
+    }
+
+    public boolean isAtirouParado() {
+        return atirouParado;
+    }
+
+    public void setAtirouParado(boolean atirouParado) {
+        this.atirouParado = atirouParado;
+    }
+    
+    
+    
+}
